ba0a35674b7744801047f0b9bc7010629cbe1974
Melanie Schipper <78080525+MSchipper91@users.noreply.github.com>
2021-08-04 16:49:42 +0200

    Add files via upload

---
+++ opfracht_1/index.js
@@ -0,0 +1,10 @@
+console.log("Hello Winc Academy");
+            let firstName = 'Melanie';
+            console.log(firstName)
+            console.log(4+4)
+            console.log('4'+'4')
+            console.log(6-3)
+            console.log(4*4)
+            console.log(6/2)
+            console.log(100%20)
+            let age = 30;
